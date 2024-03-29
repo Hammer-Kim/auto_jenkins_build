d989422ea392de1b2c8b1add01b0c57daa360955
Hammer-Kim <alsgud7718@gmail.com>
2024-03-29 17:05:54 +0900

    feat : 초기 pjt 생성

---
+++ src/main/java/com/auto/test/AutoJenkinsBuildApplication.java
@@ -0,0 +1,13 @@
+package com.auto.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoJenkinsBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoJenkinsBuildApplication.class, args);
+	}
+
+}
